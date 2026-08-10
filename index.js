72d5be5cc141811a8b9ea1038e1dcfc8b7acb420
evolucionplex-netizen <evolucionplex@gmail.com>
2026-08-10 08:21:29 +0200

    index.js

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-    const { Client, LocalAuth } = require('whatsapp-web.js');
+const { Client, LocalAuth } = require('whatsapp-web.js');
 
-    const client = new Client({
-        authStrategy: new LocalAuth({
-            dataPath: '/tmp'
-        }),
-        puppeteer: {
-            args: ['--no-sandbox', '--disable-setuid-sandbox']
-        }
-    });
+const client = new Client({
+    authStrategy: new LocalAuth({
+        dataPath: '/tmp'
+    }),
+    puppeteer: {
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    }
+});
 
-    client.on('qr', qr => {
-        console.log('Scan this QR Code with your WhatsApp:');
-        console.log(qr);
-    });
+client.on('qr', qr => {
+    console.log('Scan this QR Code with your WhatsApp:');
+    console.log(qr);
+});
 
-    client.on('ready', () => {
-        console.log('Client is ready!');
-    });
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
 
-    client.on('message', msg => {
-        if (msg.body === 'hola') {
-            msg.reply('Hola! Soy tu bot 24/7 🚀');
-        }
-    });
+client.on('message', async msg => {
+    if (msg.body === 'hola') {
+        msg.reply('Hola! Soy tu bot 24/7 🚀');
+    }
+});
 
-    client.initialize();
+client.initialize();
