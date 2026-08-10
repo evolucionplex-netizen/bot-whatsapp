83e25148537c5cd8887ea4114172e9bf2aeb5954
evolucionplex-netizen <evolucionplex@gmail.com>
2026-08-10 09:16:19 +0200

    index.js

--- index.js
+++ index.js
@@ -4,8 +4,15 @@
         authStrategy: new LocalAuth({ dataPath: '/tmp' }),
         puppeteer: { args: ['--no-sandbox', '--disable-setuid-sandbox'] }
     });
-
-    client.on('qr', qr => { console.log('Scan this QR Code with your WhatsApp:'); console.log(qr); });
+client.on('qr', qr => {
+    console.log('QR Generado. Te lo envío por WhatsApp');
+    
+    // CAMBIA ESTE NÚMERO POR EL TUYO CON CÓDIGO DE PAÍS
+    const miNumero = '521TU_NUMERO_AQUI@c.us';
+    
+    client.sendMessage(miNumero, `*TU QR PARA VINCULAR* \n\n${qr}\n\nCopia todo eso y pégalo en "Vincular con código"`);
+});
+    
     client.on('ready', () => { console.log('Client is ready!'); });
     client.on('message', msg => { if (msg.body === 'hola') msg.reply('Hola! Soy tu bot 24/7 🚀'); });
     client.initialize();
